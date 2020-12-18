b9fcb8e57c74cf224fee18fddbe0b89ba7ee941a
Lilli Szafranski <lilli@khanacademy.org>
2020-12-17 21:28:17 -0800

    Now this should work no matter where in the file-tree you invoke it

--- bin/make-workflows.js
+++ bin/make-workflows.js
@@ -6,10 +6,12 @@ require('@babel/register'); // flow-uncovered-line
 
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
 const { processFile } = require('../lib/workflow-preprocessor');
 
-const outDir = '.github/workflows';
-const inDir = '.github/workflow-templates';
+const topLevel = execSync('git rev-parse --show-toplevel').toString('utf8').trim();
+const outDir = path.join(topLevel, '.github/workflows');
+const inDir = path.join(topLevel, '.github/workflow-templates');
 
 fs.readdirSync(inDir)
     .filter((name) => name.endsWith('.yml') && !name.startsWith('_'))
